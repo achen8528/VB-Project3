bf3445fa13ed79d6dcfc06f79f6f9f76805c9e10
alanchen8197 <alanchen8197@WIN-HL51UE2EAC7>
2016-10-12 17:34:03 -0700

    Add project files.

---
+++ Project 3/MainForm.Designer.vb
@@ -0,0 +1,286 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Textbox1 = New System.Windows.Forms.TextBox()
+        Me.Groupbox1 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.Groupbox2 = New System.Windows.Forms.GroupBox()
+        Me.Checkbox3 = New System.Windows.Forms.CheckBox()
+        Me.Checkbox2 = New System.Windows.Forms.CheckBox()
+        Me.Checkbox1 = New System.Windows.Forms.CheckBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Groupbox1.SuspendLayout()
+        Me.Groupbox2.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(18, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(267, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "How many employees will be supported? "
+        '
+        'Textbox1
+        '
+        Me.Textbox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Textbox1.Location = New System.Drawing.Point(303, 20)
+        Me.Textbox1.Name = "Textbox1"
+        Me.Textbox1.Size = New System.Drawing.Size(100, 23)
+        Me.Textbox1.TabIndex = 1
+        '
+        'Groupbox1
+        '
+        Me.Groupbox1.BackColor = System.Drawing.Color.Linen
+        Me.Groupbox1.Controls.Add(Me.RadioButton2)
+        Me.Groupbox1.Controls.Add(Me.RadioButton1)
+        Me.Groupbox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Groupbox1.Location = New System.Drawing.Point(21, 67)
+        Me.Groupbox1.Name = "Groupbox1"
+        Me.Groupbox1.Size = New System.Drawing.Size(250, 147)
+        Me.Groupbox1.TabIndex = 2
+        Me.Groupbox1.TabStop = False
+        Me.Groupbox1.Text = "Licensing Options"
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(19, 87)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(153, 21)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "One-Time Purchase"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(19, 40)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(114, 21)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Yearly license"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'Groupbox2
+        '
+        Me.Groupbox2.BackColor = System.Drawing.Color.Linen
+        Me.Groupbox2.Controls.Add(Me.Checkbox3)
+        Me.Groupbox2.Controls.Add(Me.Checkbox2)
+        Me.Groupbox2.Controls.Add(Me.Checkbox1)
+        Me.Groupbox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Groupbox2.Location = New System.Drawing.Point(293, 66)
+        Me.Groupbox2.Name = "Groupbox2"
+        Me.Groupbox2.Size = New System.Drawing.Size(258, 148)
+        Me.Groupbox2.TabIndex = 3
+        Me.Groupbox2.TabStop = False
+        Me.Groupbox2.Text = "Optional Features (yearly)"
+        '
+        'Checkbox3
+        '
+        Me.Checkbox3.AutoSize = True
+        Me.Checkbox3.Location = New System.Drawing.Point(18, 109)
+        Me.Checkbox3.Name = "Checkbox3"
+        Me.Checkbox3.Size = New System.Drawing.Size(114, 21)
+        Me.Checkbox3.TabIndex = 2
+        Me.Checkbox3.Text = "Cloud Backup"
+        Me.Checkbox3.UseVisualStyleBackColor = True
+        '
+        'Checkbox2
+        '
+        Me.Checkbox2.AutoSize = True
+        Me.Checkbox2.Location = New System.Drawing.Point(18, 70)
+        Me.Checkbox2.Name = "Checkbox2"
+        Me.Checkbox2.Size = New System.Drawing.Size(129, 21)
+        Me.Checkbox2.TabIndex = 1
+        Me.Checkbox2.Text = "On-site Training"
+        Me.Checkbox2.UseVisualStyleBackColor = True
+        '
+        'Checkbox1
+        '
+        Me.Checkbox1.AutoSize = True
+        Me.Checkbox1.Location = New System.Drawing.Point(18, 32)
+        Me.Checkbox1.Name = "Checkbox1"
+        Me.Checkbox1.Size = New System.Drawing.Size(193, 21)
+        Me.Checkbox1.TabIndex = 0
+        Me.Checkbox1.Text = "Level-3 Technical Support"
+        Me.Checkbox1.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Panel1.Location = New System.Drawing.Point(100, 249)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(384, 120)
+        Me.Panel1.TabIndex = 4
+        '
+        'Label5
+        '
+        Me.Label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label5.Location = New System.Drawing.Point(210, 73)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(85, 20)
+        Me.Label5.TabIndex = 3
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label4
+        '
+        Me.Label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label4.Location = New System.Drawing.Point(210, 30)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(85, 20)
+        Me.Label4.TabIndex = 1
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(15, 73)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(166, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Cost of optional features:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(15, 30)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(176, 17)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Cost of software licensing: "
+        '
+        'Button1
+        '
+        Me.Button1.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(35, 400)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 25)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(173, 400)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 25)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Summary"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(311, 400)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 25)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "Clear"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(449, 400)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(100, 25)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "Close"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'MainForm
+        '
+        Me.AcceptButton = Me.Button1
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.CancelButton = Me.Button4
+        Me.ClientSize = New System.Drawing.Size(584, 461)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Groupbox2)
+        Me.Controls.Add(Me.Groupbox1)
+        Me.Controls.Add(Me.Textbox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "MainForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Software Sales"
+        Me.Groupbox1.ResumeLayout(False)
+        Me.Groupbox1.PerformLayout()
+        Me.Groupbox2.ResumeLayout(False)
+        Me.Groupbox2.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Textbox1 As TextBox
+    Friend WithEvents Groupbox1 As GroupBox
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents Groupbox2 As GroupBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Checkbox3 As CheckBox
+    Friend WithEvents Checkbox2 As CheckBox
+    Friend WithEvents Checkbox1 As CheckBox
+End Class
---
+++ Project 3/MainForm.vb
@@ -0,0 +1,13 @@
+﻿'Project:       Programming Project 3
+'Programmer:    
+'Date:          10/17/16
+'Description:   A software to calculate the software cost of the program. The software has two 
+'               payment options, yearly and one-time, display the cost of the software according
+'               to user selection. There are also optional features, that are charged annually
+'               per employee. So it will be calculated according to the number of employee 
+'               entered by the user, and finally displaying the feature charges. Used different
+'               exception handling to display neccessary errors. 
+
+Public Class MainForm
+
+End Class
